c17f4b3386457223fb23ac8c39d3e994df2388a8
HP <HP@Saurabh>
2023-02-15 13:01:57 +0530

    change the xpath for login button (ticket25)

--- ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
+++ ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class w3createawebsite {
-	@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
+	//@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
+	@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login1']")
 	private WebElement login;
 	 //constructor
 	public  w3createawebsite (WebDriver driver)
