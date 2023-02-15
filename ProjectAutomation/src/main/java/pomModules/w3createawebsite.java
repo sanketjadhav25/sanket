1aa3e81bafef45b5a3ab7853bbf99dd03a541ee4
sanketjadhav25 <125434731+sanketjadhav25@users.noreply.github.com>
2023-02-15 13:26:13 +0530

    Update w3createawebsite.java
    comment the xpath

--- ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
+++ ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class w3createawebsite {
-	@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
-	private WebElement login;
+	//@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
+	//private WebElement login;
 	 //constructor
 	public  w3createawebsite (WebDriver driver)
 	{
