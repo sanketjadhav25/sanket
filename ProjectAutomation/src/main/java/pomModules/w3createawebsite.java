3fd8badb23e35da3ab85a5b4c046fbca053a1cbc
HP <HP@Saurabh>
2023-02-15 13:19:33 +0530

    changr the login button

--- ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
+++ ProjectAutomation/src/main/java/pomModules/w3createawebsite.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class w3createawebsite {
-	//@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
-	@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login1']")
+	@FindBy(xpath="//button[@class='_1VfsI _OD95i _3_H0V login']")
 	private WebElement login;
 	 //constructor
 	public  w3createawebsite (WebDriver driver)
